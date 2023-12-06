0d0625775afd10f66420bb76243879971e7152ef
jhy <jianghuayu@popmart.com>
2023-12-06 19:01:22 +0800

    feat: less-loader

--- src/renderer/pages/Index/index.tsx
+++ src/renderer/pages/Index/index.tsx
@@ -1,7 +1,19 @@
 import React, { useState, useEffect } from 'react';
 
-type Iprops = {};
+import styles from './index.module.less';
 
-export default function Index(props: Iprops) {
-  return <div>134</div>;
+export default function IndexCom() {
+  return (
+    <div className={styles.container}>
+      <div className={`${styles.content} ${styles.calendarContainer}`}>
+        {/* <MyCalendar /> */}1
+      </div>
+      <div className={`${styles.content} ${styles.listContainer}`}>
+        {/* <MyList /> */}2
+      </div>
+      <div className={`${styles.content} ${styles.editContainer}`}>
+        {/* <MyEdit /> */}3
+      </div>
+    </div>
+  );
 }
